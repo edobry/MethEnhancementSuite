26b87fe78c603cc56d5cbd41081676210cbc081c
Eugene Dobry <edobry@gmail.com>
2013-10-21 16:41:38 -0400

    "Rate" overlay on seller mouseover

---
+++ mes.js
@@ -0,0 +1,41 @@
+;(function ($) {
+	window.MES = window.MES || {};
+	MES.Util = MES.Util || {};
+	var S = MES.Util.S = (function () {
+	    var cache = {};
+	    var select = function (selector, fresh) {
+	    	if(cache.hasOwnProperty(selector) && !fresh)
+	    		return cache[selector];
+	    	else {
+	    		var jObject = $(selector);
+	    		return jObject.length > 0 ? (cache[selector] = jObject) : [];
+	    	}
+	    };
+	    select.clear = function () { cache = {}; };
+	    return select;
+	})();
+
+	MES.showRate = function(rate) {
+		debugger;
+		var modal = S("#rateModal");
+		if(modal.length == 0) 
+			modal = $("<div>").attr("id", "rateModal").css({
+				"height": 18,
+				"padding": 5,
+				"display": "none",
+				"border": "1px solid black",
+				"border-radius": 5,
+				"background-color": "white",
+				"position": "fixed",
+				"top": 28,
+				"right": 21
+			}).appendTo(S(".tabs"));
+
+		modal.text("Rate: " + pretty_int(rate)).show();
+	};
+
+	S("#sellers").find(".s_div").hover(function (e){
+		var item = pd.sellers[$(this)[0].id];
+		MES.showRate(item.cost/item.rps);
+	}, function (e) { S("#rateModal").hide(); });
+})($);
